1d68ce3f6ebbace93dd15c5148b3569cc3650562
Austin <austinobejo@gmail.com>
2019-09-09 22:25:10 -0400

    Fixed mysql combatibility with emoji

--- database/setup.sql
+++ database/setup.sql
@@ -4,10 +4,7 @@ use redditquery;
 
 create table all_top_day (id INT AUTO_INCREMENT PRIMARY KEY, post_id VARCHAR(50), subreddit_id VARCHAR(20), author_id VARCHAR(20), score INT, vote_ratio FLOAT, submission_time INT, grab_time INT, is_self_post BOOLEAN, self_text LONGTEXT, post_link MEDIUMTEXT, submission_title VARCHAR(500), num_comments INT, over_18 BOOLEAN);
 
-ALTER TABLE
-    all_top_day
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+ALTER TABLE all_top_day CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'mysql';
 
